08251b7a6d692095803dab986f432f3783529219
dead_thing <mbruh4338@gmail.com>
2024-07-13 12:13:40 -0400

    stubbed blacklist function as PF2 does not have that

--- troubleshoot.py
+++ troubleshoot.py
@@ -3,9 +3,12 @@ import vars
 import gui
 import urllib.request
 
+# We don't have a blacklist system in PF2. Stub this function for now.
 def apply_blacklist():
+    '''
     gui.message(_("Applying safety blacklist..."))
     try:
         urllib.request.urlretrieve("https://tf2classic.org/serverlist/blacklist.php", vars.INSTALL_PATH + "/tf2classic/cfg/server_blacklist.txt")
     except:
         gui.message(_("WARNING: could not apply safety blacklist."))
+    '''
