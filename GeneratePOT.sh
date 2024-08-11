3df9be41fc3ed6a3c0defc89960b762e76f09959
dani <sourdanidev@gmail.com>
2024-08-11 17:21:40 -0400

    Updated documentation

--- GeneratePOT.sh
+++ GeneratePOT.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
+# Linux Only
 POT_FILE=locale/pf2-downloader.pot
 xgettext --package-name=pf2-downloader --keyword=_ --keyword=_N:1,2 -l python -o "$POT_FILE" *.py
 for PO_FILE in locale/*/LC_MESSAGES/*.po
 do
 	msgmerge -U "$PO_FILE" "$POT_FILE"
 done
+	
